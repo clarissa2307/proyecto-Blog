b9b95966eb39374334a1fceccd43a499dd8b2a7a
clarissa2307 <marthaleo07@gmail.com>
2021-02-15 21:41:30 -0600

    new

--- database/factories/PostFactory.php
+++ database/factories/PostFactory.php
@@ -4,6 +4,7 @@ namespace Database\Factories;
 
 use App\Models\Post;
 use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Str;
 
 class PostFactory extends Factory
 {
@@ -22,7 +23,9 @@ class PostFactory extends Factory
     public function definition()
     {
         return [
-            //
+            'user_id'   => 1,
+            'title'     => $this->faker->sentence,
+            'body'      => $this->faker->text(800),
         ];
     }
 }
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,11 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
+use App\Models\User;
+use App\Models\Post;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,6 +18,14 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        User::create([
+            'name' => 'Italo Morales F',
+            'email' => 'i@admin.com',
+            'password' => bcrypt('123456')
+        ]);
+
+        Post::factory()->count(24)->create();
     }
+
 }
+
